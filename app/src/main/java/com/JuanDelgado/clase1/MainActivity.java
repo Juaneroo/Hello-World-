b13d735aeb64236e45a657e9376d07b8043c52b9
Juaneroo <juaneroskate98@gmail.com>
2023-08-28 20:42:28 -0500

    first commit

--- app/src/main/java/com/JuanDelgado/clase1/MainActivity.java
+++ app/src/main/java/com/JuanDelgado/clase1/MainActivity.java
@@ -3,12 +3,25 @@ package com.JuanDelgado.clase1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mensajeVisible;
+    EditText nombreUsuario;
+    int userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mensajeVisible = findViewById(R.id.mensajeVisible);
+        nombreUsuario = findViewById(R.id.nombreUsuario);
+
+        mensajeVisible.setText("Hola señor Usuario");
+        nombreUsuario.getText();
+
+        userName = Integer.parseInt(nombreUsuario.getText().toString());
     }
 }
